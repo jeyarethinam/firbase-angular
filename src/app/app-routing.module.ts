a419f0e9dd12c3fb8de2d0780fc0527c2064f4ea
jey <jeyarethinam.net@outlook.com>
2018-10-24 08:39:59 +0530

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { CreateCustomerComponent } from './customers/create-customer/create-customer.component';
+import { CustomersListComponent } from './customers/customers-list/customers-list.component';
 
-const routes: Routes = [];
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+    { path: '', redirectTo: 'customers', pathMatch: 'full' },
+    { path: 'customers', component: CustomersListComponent },
+    { path: 'add', component: CreateCustomerComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
